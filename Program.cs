5442fe6a6a10060ee10a77d9cce971306612d3e3
magonick2 <gonzalo_cisterna@usmp.pe>
2025-09-27 05:49:35 -0500

    Corregir using statements y agregar InMemory package

--- Program.cs
+++ Program.cs
@@ -1,50 +1,15 @@
-﻿using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using portalinmobiliario1.Data;
-using portalinmobiliario1.Services;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Environment.EnvironmentName = "Production";
-// Database - Simple
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? "DataSource=app.db;Cache=Shared";
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
-
-// Identity básico
-builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-})
-.AddRoles<IdentityRole>()
-.AddEntityFrameworkStores<ApplicationDbContext>();
-
-// Cache simple
-builder.Services.AddMemoryCache();
-builder.Services.AddScoped<ICacheService, CacheService>();
-
-// Session básica
-builder.Services.AddSession();
-
+// Configuración mínima
 builder.Services.AddControllersWithViews();
-builder.Services.AddDataProtection()
-    .PersistKeysToFileSystem(new DirectoryInfo("/tmp/keys"));
 
 var app = builder.Build();
 
-// Pipeline simple
-app.UseExceptionHandler("/Home/Error");
+// Pipeline mínimo
 app.UseStaticFiles();
 app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseSession();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
 
 app.Run();
